8db1c53d0f56624e77de6e06833c48f4a60c0231
mohamedalibenchikha <mohamedali.benchikha@esprit.tn>
2023-12-26 22:39:56 +0100

    first commit

---
+++ class/config.php
@@ -0,0 +1,13 @@
+<?php
+define('DB_HOST', 'bones.dathost.net');
+define('DB_PORT', '3306');
+define('DB_NAME', '6589bd42da47767b534f06b5');
+define('DB_USER', 'ZYm9QtpHdntTTwa1');
+define('DB_PASS', 'CSBMqSrWlgQ');
+
+define('WEB_STYLE_DARK', true);
+
+define('STEAM_API_KEY', '4513F7C71321434F20C204E6A4327DC1');
+define('STEAM_DOMAIN_NAME', '');
+define('STEAM_LOGOUT_PAGE', '');
+define('STEAM_LOGIN_PAGE', '');
